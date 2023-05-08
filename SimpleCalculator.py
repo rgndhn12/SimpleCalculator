ba93ff10552091db0fcb9792e3c0572597d94291
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-05-08 11:21:30 +0800

    Pseudocode

---
+++ SimpleCalculator.py
@@ -0,0 +1,21 @@
+# Dahan, Regine Fae M. BSCPE 1-5 ( Simple Calculator: Exception Handling )
+
+#introducing the operations
+
+#each operation's calculation
+
+#user choosing the operation
+
+    #if addition
+        #print sum
+
+    #if subtraction
+        #print difference
+
+    #if multiplication
+        #print product
+
+    #if division
+        #print quotient
+
+#note: do not forget to add exception handling
