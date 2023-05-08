ac467ca217e05f126d4646ccf41594e39dcecf10
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-05-08 16:46:49 +0800

    adding features: intro and outro!

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,14 +1,26 @@
 # Dahan, Regine Fae M. BSCPE 1-5 ( Simple Calculator: Exception Handling )
 
+#introduction
+import pyfiglet
+import time
+import colorama
+from colorama import Fore
+colorama.init()
+print(Fore.BLACK+pyfiglet.figlet_format("CALCULATOR",font="stop"))
+time.sleep(1)
+border = "✪" * 75
+border_2 = " 𓇼 " * 25
+print(border)
+print(Fore.LIGHTBLUE_EX+' THIS PROGRAM WILL HELP YOU ADD, SUBTRACT, MULTIPLY or DIVIDE TWO NUMBERS... ')
+print(border)
+time.sleep(2)
+
 run_count = 0
 def main():
 
     global run_count
     run_count = run_count +1 
     try:
-
-        #introducing the operations
-        print ( " You may choose your desired Operation!\n A. Addition\n S. Subtraction\n M. Multiplication\n D. Division ")
 
         #each operation's calculation
         def A_Addition(num_1, num_2):
@@ -25,66 +37,81 @@ def main():
             
         #user choosing the operation
         while True:
-            print ( " Choose your desired operation.\n If Addition, press A. If Subtraction, press S. If Multiplication, press M. If Division, press D. ")
+            print (Fore.LIGHTMAGENTA_EX+ " CHOOSE YOUR DESIRED OPERATION.\n If Addition, press A.\n If Subtraction, press S.\n If Multiplication, press M.\n If Division, press D. ")
             user_choice = input(" >>> ").upper()
 
             if user_choice in ["A", "S", "M", "D"]:
-                num_1 = float(input( " Enter your first number: "))
-                num_2 = float(input( " Enter your second number: "))
+                print(border_2)
+                num_1 = float(input(Fore.YELLOW+ " Enter your first number: "))
+                num_2 = float(input(Fore.GREEN+ " Enter your second number: "))
+                print(border_2)
 
             #if addition
                 if user_choice == "A":
                     the_result = A_Addition(num_1, num_2)  
-
+                    print(Fore.WHITE+pyfiglet.figlet_format("Computing...",font="wavy"))
+                    time.sleep(4)
                 #print sum
-                    print(" The sum of two numbers is >>> ", the_result)
+                    print(Fore.BLUE+" The SUM of two numbers is >>> ", the_result)
+                    print(border)
                 
             #if subtraction
                 elif user_choice == "S":
                     the_result = S_Subtraction(num_1, num_2)
+                    print(Fore.WHITE+pyfiglet.figlet_format("Computing...",font="wavy"))
+                    time.sleep(4)
 
                 #print difference
-                    print(" The difference of two numbers is >>> ", the_result)
+                    print(Fore.BLUE+" The DIFFERENCE of two numbers is >>> ", the_result)
+                    print(border)
 
             #if multiplication
                 elif user_choice == "M":
                     the_result = M_Multiplication(num_1, num_2)
+                    print(Fore.WHITE+pyfiglet.figlet_format("Computing...",font="wavy"))
+                    time.sleep(4)
 
                 #print product
-                    print(" The product of two numbers is >>> ", the_result)
+                    print(Fore.BLUE+" The PRODUCT of two numbers is >>> ", the_result)
+                    print(border)
 
             #if division
                 elif user_choice == "D":
                     the_result = D_Division(num_1, num_2)
-                    # if the_result is not None:
-
+                    print(Fore.WHITE+pyfiglet.figlet_format("Computing...",font="wavy"))
+                    time.sleep(4)
+                    
                 #print quotient
-                    print(" The quotient of two numbers is >>> ", the_result)
+                    print(Fore.BLUE+" The QUOTIENT of two numbers is >>> ", the_result)
+                    print(border)
                 
                 break
             else: 
                 print (" Invalid. ")
     
+    
     except ValueError: 
-        print (" ERROR: Make sure you input a number instead of words. ")
+        print (Fore.RED+" ERROR: Make sure you input a number instead of words. ")
     except TypeError:
-        print (" ERROR: Make sure your input is correct. ")
+        print (Fore.RED+" ERROR: Make sure your input is correct. ")
     except ZeroDivisionError:
-        print (" ERROR: You cannot divide by 0. ")    
+        print (Fore.RED+" ERROR: You cannot divide by 0. ")    
     finally:
-        print (" Hey, don't forget to smile ; ) ")
+        print (Fore.MAGENTA+" Hey, don't forget to smile ; ) ")
 
 #try again?
 while True:
     if run_count != 0:
-        try_again = input (' PRESS 1 if you want to try again. PRESS 2 if otherwise. ')
+        try_again = input (Fore.BLUE+' PRESS 1 if you want to try again. PRESS 2 if otherwise. ')
     else: 
-        try_again = input (' PRESS 1 to START. ')
+        try_again = input (Fore.LIGHTBLACK_EX+' PRESS 1 to START. ')
+        print(border_2)
 
     if (try_again == '1'):
         main()
     elif (try_again == '2'):
-        print(' Thank you for using my program : ) ')
+        print(border)
+        print(Fore.BLACK+pyfiglet.figlet_format("THANK YOU!",font="utopia"))
         exit()
     else:
         print('Invalid.')
